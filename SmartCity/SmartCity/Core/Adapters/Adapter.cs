9980d7303f91f486c66a7e246b920ff66dff30f7
Asilbek-Khujanazarov <asilbekkhujanazarov@gmail.com>
2025-11-22 15:03:01 +0500

    Task is completed by Asilbek

---
+++ SmartCity/SmartCity/Core/Adapters/Adapter.cs
@@ -0,0 +1,42 @@
+﻿// Core/Adapters/Adapter.cs
+
+namespace SmartCitySystem.Core.Adapters
+{
+    // SmartCity tizimi kutgan interfeys (Target)
+    public interface ICityWeather
+    {
+        double GetCurrentTemperatureC();
+        bool IsRaining();
+    }
+
+    // Tashqi tizim (Adaptee) - Biz o'zgartira olmaydigan sinf
+    public class ThirdPartyWeatherService
+    {
+        public int FetchTemperatureKelvin() => 295;
+        public string GetPrecipitationStatus() => "yomg'ir_yo'q";
+    }
+
+    // Adapter sinfi: Tashqi tizimni ichki interfeysga moslashtiradi
+    public class ThirdPartyWeatherAdapter : ICityWeather
+    {
+        private readonly ThirdPartyWeatherService _adaptee;
+
+        public ThirdPartyWeatherAdapter(ThirdPartyWeatherService adaptee)
+        {
+            // Adapter Tashqi tizimning nusxasini ushlab turadi
+            _adaptee = adaptee;
+        }
+
+        public double GetCurrentTemperatureC()
+        {
+            // Kelvindan Selsiyga konvertatsiya mantiqi
+            return _adaptee.FetchTemperatureKelvin() - 273.15;
+        }
+
+        public bool IsRaining()
+        {
+            // String formatidan bool ga konvertatsiya mantiqi
+            return _adaptee.GetPrecipitationStatus().ToLower().Contains("yomg'ir_bor");
+        }
+    }
+}
---
+++ SmartCity/SmartCity/Modules/Security/SecuritySystem.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartCitySystem.Modules.Security
+{
+    public interface ISecurityAccess
+    {
+        void AccessCameraFeeds();
+        void ResetAlarmSystem();
+    }
+
+    // Haqiqiy Ob'ekt (Nihoyatda muhim va himoyalanishi kerak)
+    public class SecuritySystem : ISecurityAccess
+    {
+        public void AccessCameraFeeds() => Console.WriteLine("[Security] MAXFIY: Barcha kamera yozuvlariga kirish ruxsati berildi.");
+        public void ResetAlarmSystem() => Console.WriteLine("[Security] MAXFIY: Trevoga tizimi qayta tiklandi.");
+    }
+}
